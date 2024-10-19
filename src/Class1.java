475882b3cfae892feab0c3e47d853b0fe580cdc1
appu <appu.v222@gmail.com>
2024-10-19 19:48:32 +0530

    rr

---
+++ src/Class1.java
@@ -0,0 +1,20 @@
+
+public class Class1 extends Class2 {
+
+	public static void main(String[] args) {
+		
+		Class1 cl1 = new Class1();
+		cl1.p1();
+		
+		C1 ();
+		
+	
+
+	}
+	
+	public static void C1 () {
+		
+		System.out.println("This is child");
+	}
+
+}
---
+++ src/Class2.java
@@ -0,0 +1,10 @@
+
+public class Class2 {
+	
+	
+	public void p1() {
+		
+		System.out.println("This is parent");
+	}
+
+}
